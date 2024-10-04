1ccc9ff8b72c32eb90bb3350af6394410bd9dfb1
HugoGELLIDA <hugo.gellida34@gmail.com>
2024-10-04 22:05:49 +0200

    TODO done in App.js

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -3,9 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Log_in from "./pages/login";
 
 function App() {
-  // TODO: Mettre le style dans un .css
   return (
-    <div className="App" style={{ height: "100%", width: "100%", overflow: "hidden" }}>
+    <div className="App">
       <Routes>
         <Route path="/" element={<Log_in />} />
       </Routes>
